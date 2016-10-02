30b8f9185f709bdf5a8832e12c9bd454214a8fa6
hungnd1 <hungnd1@vivas.vn>
2016-10-02 10:03:05 +0700

    a

--- backend/views/news/_form.php
+++ backend/views/news/_form.php
@@ -114,7 +114,7 @@ Yii::$app->session->set('KCFINDER', $kcfOptions);
         ->andWhere(['status' => Category::STATUS_ACTIVE])->andWhere(['type'=>$type])->all(), 'id', 'display_name')) ?>
     <?php }else{ ?>
         <?= $form->field($model, 'category_id')->dropDownList(ArrayHelper::map(Category::find()
-            ->andWhere(['status' => Category::STATUS_ACTIVE])->andWhere(['type'=>$type])->all(), 'id', 'display_name'))->hiddenInput() ?>
+            ->andWhere(['status' => Category::STATUS_ACTIVE])->andWhere(['type'=>$type])->all(), 'id', 'display_name'))g) ?>
     <?php }?>
     <?= $form->field($model, 'content')->widget(\common\widgets\CKEditor::className(), [
         'options' => [
