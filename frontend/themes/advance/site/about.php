7ad24b607b292b653df4e8d6ec713e5160bc4e23
hungnd1 <hungnd1@vivas.vn>
2017-09-22 14:32:19 +0700

    a

--- frontend/themes/advance/site/about.php
+++ frontend/themes/advance/site/about.php
@@ -50,7 +50,7 @@ use yii\helpers\Url;
                             <div class="carousel-caption">
                                 <div>
                                     <h3>Dự án dịch vụ thông tin Green Coffee</h3>
-                                    Cung cấp các dịch vụ thông tin coffee. Chi tiết xem video giới thiệu tại đây<br>
+                                    Cung cấp các dịch vụ thông tin coffee. Chi tiết xem video giới thiệu tại đây.https://www.youtube.com/watch?v=yhgu77BJoAY<br>
                                 </div>
                             </div>
                         </div>
