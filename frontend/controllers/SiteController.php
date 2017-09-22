76608d269a1aa38f4af1a41bba4781f66629b952
hungnd1 <hungnd1@vivas.vn>
2017-09-22 12:25:33 +0700

    a

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -413,7 +413,7 @@ class SiteController extends BaseController
      */
     public function actionAbout()
     {
-        $listSlide = Banner::find()->all();
+        $listSlide = Banner::find()->limit(1)->all();
         return $this->render('about', ['listSlide' => $listSlide]);
     }
 
