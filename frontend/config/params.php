fd2e3615d9334ef1d9ac8238cf4459094942de2f
hungnd1 <hungnd1292@gmail.com>
2019-01-28 23:02:59 +0700

    a

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -5,6 +5,6 @@ return [
     'upload_images'=>'uploads/images',
     'page_size'=>10,
 //    'apiUrl' => 'http://45.32.112.173:84/',
-    'apiUrl' => 'http://45.119.80.32:81/',
+    'apiUrl' => 'http://thongtincafe.online/',
     'timeExpired'=> 7
 ];
